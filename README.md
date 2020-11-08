b38316fe573c85ec48018dd0ec015db9e70f50b1
Pfed-prog <fedorovn19@gmail.com>
2020-11-08 19:30:00 +0300

    WindowDressing

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ The script uses specified login and password to send requests for answer on the 
 Example:
 "py quora.py -e qwerty@gmil.com -p Abcdef1234"
 
-Thescript can also sort questions by the most recent and last day/week/month/year payments.
+The script can also sort questions by the most recent and last day/week/month/year payments.
 
 "py quora.py -e qwerty@gmil.com -p Abcdef1234 -c day"
--- quora.py
+++ quora.py
@@ -1,51 +1,56 @@
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
 from time import sleep
 import argparse
 
-path = 'C:\webdrivers\chromedriver.exe'
+#for future optimization
+#from selenium.webdriver.support import expected_conditions as EC
+#from selenium.webdriver.support.wait import WebDriverWait
+#from selenium.webdriver.common.by import By
+#from selenium.common.exceptions import StaleElementReferenceException
+
 
 class Bot:
+    
     def __init__(self, login, password, category):
-        options = webdriver.ChromeOptions()
-        options.add_argument("start-maximized")
-        options.add_argument("disable-infobars")
-        options.add_argument("--disable-extensions")
-        self.browser = webdriver.Chrome(options=options, executable_path=path)
         self.login = login
-        self.browser.get('https://www.quora.com')
+        self.password = password
         self.category = category
+        #options = webdriver.ChromeOptions()
+        #options.add_argument("start-maximized")
+        #options.add_argument("disable-infobars")
+        #options.add_argument("--disable-extensions")
+        self.driver = webdriver.Chrome(executable_path=f'chromedriver.exe')
+        self.driver.get('https://www.quora.com')
         try:
-            self.browser.find_element_by_xpath("//input[@placeholder=\'Email\']").send_keys(login)
-            self.browser.find_element_by_xpath("//input[@placeholder=\'Password\']").send_keys(password)
-            self.browser.find_element_by_xpath("//input[@value=\'Login\']").click()
+            self.driver.find_element_by_xpath("//input[@placeholder=\'Email\']").send_keys(login)
+            self.driver.find_element_by_xpath("//input[@placeholder=\'Password\']").send_keys(password)
+            self.driver.find_element_by_xpath("//input[@value=\'Login\']").click()
         except:
-            self.browser.find_element_by_xpath("//input[@placeholder=\"Your email\"]").send_keys(login)
-            self.browser.find_element_by_xpath("//input[@placeholder=\"Your password\"]").send_keys(password)
-            self.browser.find_element_by_xpath("//button[@tabindex=\"4\"]").click()
+            self.driver.find_element_by_xpath("//input[@placeholder=\"Your email\"]").send_keys(login)
+            self.driver.find_element_by_xpath("//input[@placeholder=\"Your password\"]").send_keys(password)
+            self.driver.find_element_by_xpath("//button[@tabindex=\"4\"]").click()
 
     def ask(self):
         sleep(2)
-        link = "https://www.quora.com/partners?sort_by={}#questions".format(self.category)
-        self.browser.get(link)
-        self.browser.implicitly_wait(2)
-        questions = self.browser.find_element_by_id("questions").find_element_by_css_selector("div.paged_list_wrapper").find_elements_by_css_selector("div.QuestionListItem")
-        self.browser.implicitly_wait(3)
+        self.driver.get("https://www.quora.com/partners?sort_by={}#questions".format(self.category))
+        sleep(2)
+        questions = self.driver.find_element_by_id("questions").find_element_by_class_name("paged_list_wrapper").find_elements_by_class_name("QuestionListItem.partners_question_list_item")    
+        self.driver.implicitly_wait(3)
         for x in range(10):
-            self.browser.implicitly_wait(2)
+            self.driver.implicitly_wait(2)
             questions[x].find_element_by_css_selector("div.a2a_section").find_element_by_css_selector("span").click()
-            self.browser.implicitly_wait(2)
-            a = self.browser.find_elements_by_class_name("q-box.qu-py--small.qu-borderBottom.qu-hover--bg--undefined.qu-tapHighlight--none")
-            self.browser.implicitly_wait(10)
+            self.driver.implicitly_wait(2)
+            a = self.driver.find_elements_by_class_name("q-box.qu-py--small.qu-borderBottom.qu-hover--bg--undefined.qu-tapHighlight--none")
+            self.driver.implicitly_wait(10)
             try:
                 for i in range(25):
-                    self.browser.implicitly_wait(1)
+                    self.driver.implicitly_wait(1)
                     a[i].find_element_by_class_name("q-box.qu-flex--none.qu-display--inline-flex.qu-ml--medium").find_element_by_css_selector("span").click()
                     
-                self.browser.find_element_by_class_name("q-text.qu-ellipsis.qu-whiteSpace--nowrap").click()
+                self.driver.find_element_by_class_name("q-text.qu-ellipsis.qu-whiteSpace--nowrap").click()
             except:
                 print('pushing Done')
-                self.browser.find_element_by_class_name("q-text.qu-ellipsis.qu-whiteSpace--nowrap").click()
+                self.driver.find_element_by_class_name("q-text.qu-ellipsis.qu-whiteSpace--nowrap").click()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
