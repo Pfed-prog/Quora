347ba64f66a85929ad12f71d973b2d072ef2a830
Pfed-prog <fedorovn19@gmail.com>
2020-11-08 20:34:03 +0300

    Fix: WebDriverWait

--- quora.py
+++ quora.py
@@ -1,13 +1,10 @@
 from selenium import webdriver
-from time import sleep
+#from time import sleep
 import argparse
 
-#for future optimization
-#from selenium.webdriver.support import expected_conditions as EC
-#from selenium.webdriver.support.wait import WebDriverWait
-#from selenium.webdriver.common.by import By
-#from selenium.common.exceptions import StaleElementReferenceException
-
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
 
 class Bot:
     
@@ -15,11 +12,11 @@ class Bot:
         self.login = login
         self.password = password
         self.category = category
-        #options = webdriver.ChromeOptions()
-        #options.add_argument("start-maximized")
-        #options.add_argument("disable-infobars")
-        #options.add_argument("--disable-extensions")
-        self.driver = webdriver.Chrome(executable_path=f'chromedriver.exe')
+        options = webdriver.ChromeOptions()
+        options.add_argument("start-maximized")
+        options.add_argument("disable-infobars")
+        options.add_argument("--disable-extensions")
+        self.driver = webdriver.Chrome(options = options, executable_path=f'chromedriver.exe')
         self.driver.get('https://www.quora.com')
         try:
             self.driver.find_element_by_xpath("//input[@placeholder=\'Email\']").send_keys(login)
@@ -31,25 +28,21 @@ class Bot:
             self.driver.find_element_by_xpath("//button[@tabindex=\"4\"]").click()
 
     def ask(self):
-        sleep(2)
+        WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.ID, "root")))
         self.driver.get("https://www.quora.com/partners?sort_by={}#questions".format(self.category))
-        sleep(2)
+        WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.ID, "questions")))      
         questions = self.driver.find_element_by_id("questions").find_element_by_class_name("paged_list_wrapper").find_elements_by_class_name("QuestionListItem.partners_question_list_item")    
-        self.driver.implicitly_wait(3)
+        WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, "icon_svg-stroke")))
         for x in range(10):
-            self.driver.implicitly_wait(2)
             questions[x].find_element_by_css_selector("div.a2a_section").find_element_by_css_selector("span").click()
-            self.driver.implicitly_wait(2)
-            a = self.driver.find_elements_by_class_name("q-box.qu-py--small.qu-borderBottom.qu-hover--bg--undefined.qu-tapHighlight--none")
-            self.driver.implicitly_wait(10)
             try:
+                WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, "q-box.qu-py--small.qu-borderBottom.qu-hover--bg--undefined.qu-tapHighlight--none")))
+                a = self.driver.find_elements_by_class_name("q-box.qu-py--small.qu-borderBottom.qu-hover--bg--undefined.qu-tapHighlight--none")
+                WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, "span")))
                 for i in range(25):
-                    self.driver.implicitly_wait(1)
                     a[i].find_element_by_class_name("q-box.qu-flex--none.qu-display--inline-flex.qu-ml--medium").find_element_by_css_selector("span").click()
-                    
-                self.driver.find_element_by_class_name("q-text.qu-ellipsis.qu-whiteSpace--nowrap").click()
+                    self.driver.execute_script("window.scrollTo(0, 300)")
             except:
-                print('pushing Done')
                 self.driver.find_element_by_class_name("q-text.qu-ellipsis.qu-whiteSpace--nowrap").click()
 
 if __name__ == '__main__':
