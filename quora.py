fc15cb4576cd930192369883640c2b02273f021c
Pavel Fedotov <66903336+Pfed-prog@users.noreply.github.com>
2020-11-19 01:22:45 +0300

    Improved number of requests; less bugs

--- quora.py
+++ quora.py
@@ -10,8 +10,8 @@ class Bot:
     def __init__(self, login, password):
         self.login = login
         self.password = password
-        self.driver = webdriver.Chrome(executable_path=f'chromedriver.exe')
-        self.driver.get('https://www.quora.com')
+        self.driver = webdriver.Chrome(executable_path=f'chromedriver.exe') #loads webdriver
+        self.driver.get('https://www.quora.com') #opens quora website
         try:
             self.driver.find_element_by_xpath("//input[@placeholder=\'Email\']").send_keys(login)
             self.driver.find_element_by_xpath("//input[@placeholder=\'Password\']").send_keys(password)
@@ -50,28 +50,42 @@ class Bot:
         questions = self.driver.find_element_by_id("questions").find_element_by_class_name("paged_list_wrapper").find_elements_by_class_name("QuestionListItem.partners_question_list_item")    
         WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, "icon_svg-stroke")))
         for x in range(low, high):
-            questions[x].find_element_by_css_selector("div.a2a_section").find_element_by_css_selector("span").click()
             try:
-                WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, "q-box.qu-py--small.qu-borderBottom.qu-hover--bg--undefined.qu-tapHighlight--none")))
-                a = self.driver.find_elements_by_class_name("q-box.qu-py--small.qu-borderBottom.qu-hover--bg--undefined.qu-tapHighlight--none")
-                WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, "span")))
-                self.driver.execute_script("window.scrollTo(0, 500)")
-                for i in range(10):
-                    a[i].find_element_by_class_name("q-box.qu-flex--none.qu-display--inline-flex.qu-ml--medium").find_element_by_css_selector("span").click()
-                find_element_by_class_name("q-text.qu-ellipsis.qu-whiteSpace--nowrap").click()
+                questions[x].find_element_by_css_selector("div.a2a_section").find_element_by_css_selector("span").click()
+                try:
+                    try:    
+                        WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, "q-box.qu-py--small.qu-borderBottom.qu-hover--bg--undefined.qu-tapHighlight--none")))
+                        a = self.driver.find_elements_by_class_name("q-box.qu-py--small.qu-borderBottom.qu-hover--bg--undefined.qu-tapHighlight--none")
+                        WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, "span")))
+                        self.driver.execute_script("window.scrollTo(0, 500)")
+                        for i in range(10):
+                            a[i].find_element_by_class_name("q-box.qu-flex--none.qu-display--inline-flex.qu-ml--medium").find_element_by_css_selector("span").click()
+                        find_element_by_class_name("q-text.qu-ellipsis.qu-whiteSpace--nowrap").click()
+                    except:
+                        topics = self.driver.find_elements_by_class_name("q-click-wrapper.qu-display--block.qu-tapHighlight--white.qu-cursor--pointer")
+                        #changes topic when there is no suggestions
+                        topics[2].find_element_by_class_name("q-box.qu-py--tiny.qu-hover--bg--undefined.qu-tapHighlight--none").click()
+                        WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, "q-box.qu-py--small.qu-borderBottom.qu-hover--bg--undefined.qu-tapHighlight--none")))
+                        a = self.driver.find_elements_by_class_name("q-box.qu-py--small.qu-borderBottom.qu-hover--bg--undefined.qu-tapHighlight--none")
+                        WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, "span")))
+                        self.driver.execute_script("window.scrollTo(0, 500)")
+                        for i in range(10):
+                            a[i].find_element_by_class_name("q-box.qu-flex--none.qu-display--inline-flex.qu-ml--medium").find_element_by_css_selector("span").click()
+                        find_element_by_class_name("q-text.qu-ellipsis.qu-whiteSpace--nowrap").click()
+                except:
+                    self.driver.find_element_by_class_name("q-text.qu-ellipsis.qu-whiteSpace--nowrap").click()
             except:
-                self.driver.find_element_by_class_name("q-text.qu-ellipsis.qu-whiteSpace--nowrap").click()
+                x+=1
     
-
-    def finish(self):
+    def finish(self): #closes the browser
         self.driver.close()
 
-my_bot = Bot('', '') #Enter Creds Here
+my_bot = Bot('Qwerty@gmail.com', 'Password') #Enter Creds Here
 my_bot.ask('recent', 0, 10, 0)
 my_bot.ask('day', 0, 10, 0)
 my_bot.ask('week', 0, 10, 0)
-#my_bot.ask('recent', 10, 20, 1)
-#my_bot.ask('day', 10, 20, 1)
-#my_bot.ask('week', 10, 20, 1)
-#my_bot.ask('day', 20, 30, 2)
+my_bot.ask('recent', 10, 20, 1)
+my_bot.ask('day', 10, 20, 1)
+my_bot.ask('week', 10, 20, 1)
+my_bot.ask('day', 20, 30, 2)
 my_bot.finish()
